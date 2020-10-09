eec466f89d76954545d303d98716261b96757155
Oceane Patiny <oceane@patiny.com>
2020-10-09 12:11:15 +0200

    chore: enhance harmonic oscillations

--- 2020-10-07/harmonicOscillation.c
+++ 2020-10-07/harmonicOscillation.c
@@ -5,9 +5,11 @@
 #include <stdlib.h>
 
 int main(void) {
+  srand(time(0));  // to do once, we "seed" the rand function
+
   printf(
       "This script returns the value of a function with 4 parameters: "
-      "A*sin(omega * t + phi).\n");
+      "A*sin(omega * t + phi) + noise.\n");
 
   double amplitude;
   printf("Enter the amplitude (A): \n");
@@ -17,18 +19,20 @@ int main(void) {
   printf("Enter the anglular frequency in Hz (omega): \n");
   scanf("%lf", &omega);
 
-  double time;
+  double t;
   printf("Enter the time in seconds (t): \n");
-  scanf("%lf", &time);
+  scanf("%lf", &t);
 
   double phi;
   printf("Enter the phaseShift in degrees (phi): \n");
-  scanf("%lf", &phi);   
+  scanf("%lf", &phi);
+
+  double noise = amplitude * rand() / RAND_MAX / 10;
+  double result = amplitude * cos((omega * t + phi) * M_PI / 180) + noise;
 
   // converting degrees to rad
   printf("The result of the operation %lf * cos(%lf * %lf + %lf) is %lf",
-         amplitude, omega, time, phi,
-         amplitude * cos((omega * time + phi)) * M_PI / 180);
+         amplitude, omega, t, phi, result);
 
   printf("\n");
   return EXIT_SUCCESS;
