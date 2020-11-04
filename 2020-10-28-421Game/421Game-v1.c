d0a64a445c561f99cba27e6c7ee85ed374ff470c
Oceane Patiny <oceane@patiny.com>
2020-11-04 04:08:24 +0100

    enhanced 421 game code (using arrays)

--- 2020-10-28-421Game/421Game-v1.c
+++ 2020-10-28-421Game/421Game-v1.c
@@ -10,18 +10,77 @@
 #define NUMBER_DICES 3
 
 // prototypes
-int diceSimulator(void);
+int gameStart(int* continueGame_p);  // prints text for user interface
+int diceSimulator(void);             // simulates dice
+int comparator(const void* p1, const void* p2);  // for sort function
+void printDices(int* dices_p);                   // print dices values
+void printStats(int* stats_p);  // prints stats of the dices thrown
 
 int main(void) {
+  srand(time(0));
   int continueGame = 1;
   int youWin = 0;
 
-  printf(
-      "Welcome to the 421 game! Enter 2 to see the rules and then start a new "
-      "game, enter 1 to start a new game.\n");
-  scanf("%i", &continueGame);
+  gameStart(&continueGame);
 
-  if (continueGame == 2) {
+  int dices[NUMBER_DICES];
+  int dicesWithIndex[NUMBER_DICES];  // array in which we encode the dice
+                                     // position in the original array on low
+                                     // weight bits
+  int stats[6] = {0};  // array to count occurences of every dice value
+
+  for (int i = 0; i < NUMBER_DICES; i++) {
+    dices[i] = diceSimulator();
+    stats[dices[i] - 1]++;
+  }
+
+  while (continueGame) {
+    printDices(dices);
+    for (int i = 0; i < NUMBER_DICES; i++) {
+      dicesWithIndex[i] = dices[i] << 2 | i;
+    }
+    qsort(dicesWithIndex, NUMBER_DICES, sizeof(int), comparator);
+
+    if (dicesWithIndex[0] >> 2 == 1 && dicesWithIndex[1] >> 2 == 2 &&
+        dicesWithIndex[2] >> 2 == 4) {
+      continueGame = 0;
+      youWin = 1;
+    } else {
+      int previous = 0;
+      for (int i = 0; i < NUMBER_DICES; i++) {
+        int currentDice = dicesWithIndex[i] >> 2;
+        if (currentDice == previous ||
+            (currentDice != 1 && currentDice != 2 && currentDice != 4)) {
+          previous = currentDice;
+          dices[dicesWithIndex[i] & 3] = diceSimulator();
+          stats[dices[i] - 1]++;
+        }
+      }
+      printf("Do you want to continue the game (0 = no, 1 = yes)? ");
+      scanf("%i", &continueGame);
+    }
+  }
+
+  if (youWin == 0) {
+    printf("YOU LOST! :( \n");
+    printStats(stats);
+  } else if (youWin == 1) {
+    printf("YOU WON! :) \n");
+    printStats(stats);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int gameStart(int* continueGame_p) {
+  printf(
+      "Welcome to the 421 game! Enter: \n"
+      "- 0 : exit program\n"
+      "- 1 : start a new game\n"
+      "- 2 : see the rules and then start a new game\n");
+  scanf("%i", continueGame_p);
+
+  if (*continueGame_p == 2) {
     printf(
         "Rules:\n "
         "- In the first turn, you will throw three dices\n "
@@ -29,33 +88,12 @@ int main(void) {
         "- If the values are not right the first time, the dices that are not "
         "correct are thrown again\n"
         "- You can repeat this until you get the right combination \n \n ");
-    continueGame = 1;
+    *continueGame_p = 1;
   }
-
-  int dice1 = diceSimulator();
-  int dice2 = diceSimulator();
-  int dice3 = diceSimulator();
-
-  while (continueGame) {
-    // the only combination where the XOR of all dices is 7 is when they have
-    // values 4, 2 and 1!
-    if (dice1 ^ dice2 ^ dice3 == 7) {
-      youWin = 1;
-    } else {
-      if (dice1 != 1 && dice1 != 2 && dice1 != 3) {
-        dice1 = diceSimulator();
-      }
-      if (dice2 != 1 && dice2 != 2 && dice2 != 3 || dice2 == dice1) {
-        dice2 = diceSimulator();
-      }
-      if (dice3 != 1 && dice3 != 2 && dice3 != 3 || dice3 == dice1 ||
-          dice3 == dice2) {
-        dice3 = diceSimulator();
-      }
-    }
+  if (*continueGame_p == 0) {
+    printf("Goodbye!\n");
+    exit(0);
   }
-
-  return EXIT_SUCCESS;
 }
 
 /* diceSimulator(): returns integer in range [1; 6]
@@ -66,8 +104,23 @@ int main(void) {
   Author: Océane Patiny - 2020.10.28
  */
 int diceSimulator(void) {
-  srand(time(0));
   return rand() % 6 + 1;
 }
 
-// *(int[NUMBER_DICES])throwDices(){};
+// The comparator compares two values (in this case, we'll sort in ascending
+// order). The result is passed to the sort function. This function comes from:
+// https://www.educative.io/edpresso/what-is-the-qsort-function-in-c
+int comparator(const void* p1, const void* p2) {
+  return (*(int*)p1 - *(int*)p2);
+}
+
+void printDices(int* dices_p) {
+  printf("Your dices are: %i %i %i \n", dices_p[0], dices_p[1], dices_p[2]);
+}
+
+void printStats(int* stats_p) {
+  printf("Dice value  Occurences");
+  for (int i = 0; i < 6; i++) {
+    printf("%i %i", i + 1, stats_p[i]);
+  }
+}
