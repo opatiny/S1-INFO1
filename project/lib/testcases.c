a29af9e8c0e8f9f0031e7af794616fc21e0fa377
Oceane Patiny <oceane@patiny.com>
2020-12-11 11:34:55 +0100

    fix (testCases): change to nbCasesFailing

--- project/lib/testcases.c
+++ project/lib/testcases.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int shouldBeEqual_i(int* testState, int value, int expected) {
+int shouldBeEqual_i(int* nbCasesFailing, int value, int expected) {
   if (value != expected) {
     printf("    got: %i, expected: %i \n", value, expected);
-    (*testState--);
+    (*nbCasesFailing)++;
     return 1;
   }
   return 0;
 }
 
-int shouldBeEqual_d(int* testState,
+int shouldBeEqual_d(int* nbCasesFailing,
                     double value,
                     double expected,
                     double maxError) {
   if (abs(value - expected) > maxError) {
     printf("    got: %lf, expected: %lf \n", value, expected);
-    (*testState)--;
+    (*nbCasesFailing)++;
     return 1;
   }
   return 0;
