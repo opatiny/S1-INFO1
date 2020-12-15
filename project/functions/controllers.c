9aae9ed7767a79fe1e1d1e58911bc15328607afe
Oceane Patiny <oceane@patiny.com>
2020-12-15 18:39:28 +0100

    feat (controllers): implementing ph control

--- project/functions/controllers.c
+++ project/functions/controllers.c
@@ -4,14 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "aquarium.h"
 #include "controllers.h"
+
+// there could be many pumps in case they are many aquariums
+PUMP pumps[2] = {{.currentValue = 100, .phThreshold = 6.5, .factor = 10}};
 
 int temperatureControl(void) {
   // printf("room temperature controlled\n");
   return 0;
 }
 
-int phControl(void) {
-  // printf("aquarium PH controlled\n");
+int phControl(int index) {
+  double currentPH = getPH(index);
+  pumps[index].currentValue =
+      pumps[index].factor * (currentPH - pumps[index].phThreshold);
+  setPumpValue(index, pumps[index].currentValue);
   return 0;
+}
+
+double getPumpValue(int index) {
+  return pumps[index].currentValue;
 }
--- project/functions/controllers.h
+++ project/functions/controllers.h
@@ -1,7 +1,15 @@
 #ifndef CONTROLLERS_H
 #define CONTROLLERS_H
 
+// STRUCTURES
+typedef struct pump {
+  double currentValue;  // in ??
+  double phThreshold;   // from 0 to 1
+  double factor;        // no dimension
+} PUMP;
+
+// PUBLIC FUNCTIONS
 int temperatureControl(void);
-int phControl(void);
+int phControl(int index);
 
 #endif
