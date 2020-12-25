3ba269e96efc25534a996ad00a39c757b4217c42
Oceane Patiny <oceane@patiny.com>
2020-12-25 15:05:47 +0100

    feat (Scheduler): start formatting output data

--- project/functions/Scheduler.c
+++ project/functions/Scheduler.c
@@ -14,8 +14,8 @@
 // DEFINES
 // our basic time unit is the second
 #define TIC_LENGTH \
-  60 * 60               // in seconds (one hour) -> accuracy of the simulation
-#define NUMBER_TICS 24  // defines the simulation length
+  60 * 60              // in seconds (one hour) -> accuracy of the simulation
+#define NUMBER_TICS 2  // defines the simulation length
 
 // defining the number of tics to wait between each cal of every function
 #define WEATHER_LIGHT_SAMPLING 1
@@ -31,11 +31,23 @@
 #define NB_ROOMS 2
 #define NB_AQUARIUMS 1
 
+// PROTOTYPES
+void printHeader(void);
+void printDataLine(DATA* data);
+
+// STRUCTURES INITIALIZATION
+DATA data;
+
+// FUNCTIONS
 int Scheduler(void) {
-  printf("inside scheduler\n");
+  printf("Welcome to this basic domotics simulator.\n");
+
+  printf("The TIC length is %i seconds.\n\n", TIC_LENGTH);
+
+  printHeader();
 
   for (int currentTIC = 1; currentTIC < NUMBER_TICS + 1; currentTIC++) {
-    printf("\nTIC %i\n", currentTIC);
+    data.TIC = currentTIC;
 
     u_int32_t currentTimeOfDay =
         currentTIC * TIC_LENGTH %
@@ -45,25 +57,27 @@ int Scheduler(void) {
     if (!(currentTIC % WEATHER_LIGHT_SAMPLING)) {
       updateWeatherLuminosity(currentTimeOfDay, LAUSANNE_SUMMER);
       double weatherLuminosity = getWeatherLuminosity(LAUSANNE_SUMMER);
-      printf("weather luminosity updated: %.2lf \n", weatherLuminosity);
+      data.weatherLuminosity = weatherLuminosity;
     }
     if (!(currentTIC % WEATHER_TEMP_SAMPLING)) {
       updateWeatherTemperature(currentTimeOfDay, LAUSANNE_SUMMER);
       double weatherTemperature = getWeatherTemperature(LAUSANNE_SUMMER);
-      printf("weather temperature updated: %.2lf \n", weatherTemperature);
+      data.weatherTemperature = weatherTemperature;
     }
     if (!(currentTIC % PROBE_TEMP_SAMPLING)) {
       for (int i = 0; i < NB_ROOMS; i++) {
         updateRoomTemperature(i);
         double roomTemperature = getRoomTemperature(i);
-        printf("temperature of room %i updated: %.2lf \n", i, roomTemperature);
+        // printf("temperature of room %i updated: %.2lf \n", i +
+        // 1, roomTemperature);
       }
     }
     if (!(currentTIC % PROBE_LIGHT_SAMPLING)) {
       for (int i = 0; i < NB_ROOMS; i++) {
         updateRoomLuminosity(i);
         double roomLuminostiy = getRoomLuminosity(i);
-        printf("luminosity of room %i updated: %.2lf \n", i, roomLuminostiy);
+        // printf("luminosity of room %i updated: %.2lf \n", i + 1,
+        //       roomLuminostiy);
       }
     }
     if (!(currentTIC % CONTROLLER_LIGHT_SAMPLING)) {
@@ -73,13 +87,27 @@ int Scheduler(void) {
       for (int i = 0; i < NB_AQUARIUMS; i++) {
         phControl(i);
         double aquariumPH = getPH(i);
-        printf("PH of aquarium %i updated: %.2lf \n", i, aquariumPH);
+        // printf("PH of aquarium %i updated: %.2lf \n", i + 1, aquariumPH);
       }
     }
     if (!(currentTIC % DATA_HANDLER_SAMPLING)) {
       dataHandler();
     }
+    printDataLine(&data);
   }
 
   return 0;
+}
+
+void printHeader(void) {
+  printf("%5s %15s %15s %15s %15s %15s %15s %15s \n", "TIC", "weather temp.",
+         "weather lum.", "room 1 temp.", "room 1 lum.", "room 2 temp.",
+         "room2 lum.", "aquarium 1 PH");
+}
+
+void printDataLine(DATA* data) {
+  printf("%5i %15d %15d %15d %15d %15d %15d %15d \n", data->TIC,
+         data->weatherTemperature, data->weatherLuminosity,
+         data->temperatureRoom1, data->luminosityRoom1, data->temperatureRoom1,
+         data->luminosityRoom1, data->aquariumPH);
 }
--- project/functions/Scheduler.h
+++ project/functions/Scheduler.h
@@ -3,4 +3,15 @@
 
 int Scheduler(void);
 
+typedef struct data {
+  int TIC;
+  double weatherTemperature;
+  double weatherLuminosity;
+  double temperatureRoom1;
+  double luminosityRoom1;
+  double temperatureRoom2;
+  double luminosityRoom2;
+  double aquariumPH
+} DATA;
+
 #endif
