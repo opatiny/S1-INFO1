3a9fc8253e670906fc9258cc0de25822d09f0629
Oceane Patiny <oceane@patiny.com>
2021-01-13 11:11:21 +0100

    feat (dataHandler): add messages structs

--- project/functions/dataHandler.h
+++ project/functions/dataHandler.h
@@ -1,6 +1,28 @@
 #ifndef DATA_HANDLER_H
 #define DATA_HANDLER_H
 
+// STRUCTURES
+typedef struct msg1 {
+  int type;
+  int controllerCode;
+  char address[45];
+} MSG1;
+
+typedef struct msg2 {
+  int type;
+  int controllerCode;
+  int phenomenomCode;
+  char phenomenomName[20];
+} MSG2;
+
+typedef struct msg3 {
+  int type;
+  int controllerCode;
+  int phenomenomCode;
+  u_int64_t timestamp;
+  float measurement;
+} MSG3;
+
 // PUBLIC FUNCTIONS
 int dataHandler(void);
 
