0273a0a55098993835ce3032198de034df27ed2e
Oceane Patiny <oceane@patiny.com>
2021-01-13 10:38:26 +0100

    feat: convert float to string

---
+++ 2020-11-13-chars/floatStringConversion.c
@@ -0,0 +1,20 @@
+// testing string functions
+
+#define _USE_MATH_DEFINES
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  double Double1 = 23235.6;
+  char s1[11];
+  sprintf(s1, "%5.3f", Double1);
+  printf("valeur du string: %s\n", s1);
+
+  char s2[] = "12345.56";
+  double Double2;
+  sscanf(s2, "%lf", &Double2);
+  printf("valeur du double: %lf\n", Double2);
+  return EXIT_SUCCESS;
+}
