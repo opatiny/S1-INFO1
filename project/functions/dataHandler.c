7e5306dd3bdf6c83772504104ca2666dff1d4c02
Oceane Patiny <oceane@patiny.com>
2021-01-13 12:03:39 +0100

    feat (dataHander) add convert_to_MSG1()'

--- project/functions/dataHandler.c
+++ project/functions/dataHandler.c
@@ -1,39 +1,46 @@
 #define _USE_MATH_DEFINES
 
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "dataHandler.h"
 
 // DEFINES
-#define MSG_MAX_LENGTH 51
-#define BUFFER_SIZE 50
-
-// STRUCTURES
-typedef char MSG[MSG_MAX_LENGTH];
 
 // PROTOTYPES
 
 // STRUCTURES INITIALIZATION
+// defining an array of messages
+MSG buffer[BUFFER_SIZE];
+int writeIndex = 0;
+int readIndex = 0;
 
 // FUNCTIONS
-int dataHandler(void) {
-  // defining an array of messages
-  MSG buffer[BUFFER_SIZE];
-  int writeIndex = 0;
-  int readIndex = 0;
+
+int convert_to_MSG1(MSG message, int controllerCode, char address[45]) {
+  if (controllerCode > 9999) {
+    return 1;
+  } else if (sizeof(address) > 46) {
+    return 1;
+  }
+
+  sprintf(message, "%s%4i%45s", "1", controllerCode, address);
 
   return 0;
 }
 
 int write_MSG1(int controllerCode, char address[45]) {
-  MSG msg1 = "1";
+  MSG msg1 = "";
+
+  convert_to_MSG1(msg1, controllerCode, address);
+  stpcpy(buffer[writeIndex], msg1);
+  writeIndex++;
 
   return 0;
 }
 
-int read_MSG1(void) {
+int read_MSG1(MSG1* message) {
   return 0;
 }
 
--- project/functions/dataHandler.h
+++ project/functions/dataHandler.h
@@ -1,18 +1,32 @@
 #ifndef DATA_HANDLER_H
 #define DATA_HANDLER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+// DEFINES
+#define MSG_MAX_LENGTH 51
+#define BUFFER_SIZE 50
+#define CODE_MAX_VALUE 9999
+#define MAX_ADDRESS_LENGTH 46
+#define MAX_PHENOMENOM_NAME_LENGTH 21
+#define MAX_TIMESTAMP_VALUE 999999999
+#define MAX_MEASUREMENT_ABS_VALUE 99999.999
+
 // STRUCTURES
+typedef char MSG[MSG_MAX_LENGTH];
+
 typedef struct msg1 {
   int type;
   int controllerCode;
-  char address[45];
+  char address[MAX_ADDRESS_LENGTH];
 } MSG1;
 
 typedef struct msg2 {
   int type;
   int controllerCode;
   int phenomenomCode;
-  char phenomenomName[20];
+  char phenomenomName[MAX_PHENOMENOM_NAME_LENGTH];
 } MSG2;
 
 typedef struct msg3 {
@@ -25,5 +39,9 @@ typedef struct msg3 {
 
 // PUBLIC FUNCTIONS
 int dataHandler(void);
+
+#ifdef TEST_ENVIRONMENT
+int convert_to_MSG1(MSG message, int controllerCode, char address[45]);
+#endif  // TEST_ENVIRONMENT
 
 #endif
